2485e35cc9d3dd88fe19d6221416be862cff3e00
Konstantin <vasilevki@gmail.com>
2022-07-05 16:30:21 +0300

    function name fixed

--- api/views.py
+++ api/views.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, jsonify
-from utils import load_posts_from_source, get_post_by_pk, get_posts_all
+from utils import get_posts_all, get_post_by_pk
 
 
 api_blueprint = Blueprint('api_blueprint', __name__)
 
 
 @api_blueprint.route('/api/posts', methods=['GET'])
-def get_posts_all():
-    posts: list[dict] = load_posts_from_source()
+def api_get_posts_all():
+    posts: list[dict] = get_posts_all()
     return jsonify(posts)
 
 
 @api_blueprint.route('/api/posts/<int:post_id>', methods=['GET'])
-def get_post_by_id(post_id: int):
+def api_get_post_by_id(post_id: int):
     post: dict = get_post_by_pk(post_id)
     return jsonify(post)
