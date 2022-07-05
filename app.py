479da878a9af7076588782665dede76bd90c67fd
Konstantin <vasilevki@gmail.com>
2022-07-05 14:47:30 +0300

    errorhandler for 404 and 500 added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from main.views import main_blueprint
 from post.views import post_blueprint
 from search.views import search_blueprint
@@ -11,6 +11,16 @@ app.register_blueprint(main_blueprint)
 app.register_blueprint(post_blueprint)
 app.register_blueprint(search_blueprint)
 app.register_blueprint(user_feed_blueprint)
+
+
+@app.errorhandler(404)
+def page_not_found_error(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
 
 
 app.run()
