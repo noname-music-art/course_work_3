b760e3ab58e7547c0516572c0c79ce837e2b2fc5
Konstantin <vasilevki@gmail.com>
2022-07-06 16:05:59 +0300

    logging added

--- api/views.py
+++ api/views.py
@@ -1,5 +1,10 @@
+import logging.config
+
 from flask import Blueprint, jsonify
 from utils import get_posts_all, get_post_by_pk
+
+logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 
 api_blueprint = Blueprint('api_blueprint', __name__)
@@ -7,11 +12,13 @@ api_blueprint = Blueprint('api_blueprint', __name__)
 
 @api_blueprint.route('/api/posts', methods=['GET'])
 def api_get_posts_all():
+    logger.info('Get all posts by API')
     posts: list[dict] = get_posts_all()
     return jsonify(posts)
 
 
 @api_blueprint.route('/api/posts/<int:post_id>', methods=['GET'])
 def api_get_post_by_id(post_id: int):
+    logger.info(f'post {post_id}')
     post: dict = get_post_by_pk(post_id)
     return jsonify(post)
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import logging.config
+
 from flask import Flask, render_template
 
 from api.views import api_blueprint
@@ -5,6 +7,9 @@ from main.views import main_blueprint
 from post.views import post_blueprint
 from search.views import search_blueprint
 from user_feed.views import user_feed_blueprint
+
+logging.config.fileConfig('logging.ini', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
@@ -18,11 +23,13 @@ app.register_blueprint(api_blueprint)
 
 @app.errorhandler(404)
 def page_not_found_error(e):
+    logger.error('404')
     return render_template('404.html'), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    logger.error('500')
     return render_template("500.html"), 500
 
 
